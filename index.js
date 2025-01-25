df67434ad1377b2dfb9e1f0703fb8031565deb78
Sam M <dmsammmusic@gmail.com>
2025-01-25 16:21:59 +0530

    add blog-2 and ui  Enhancement

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ function toggleMenu() {
 	const menu = document.getElementById('mobile-menu');
 	menu.classList.toggle('hidden');
 }
+// Update the current year dynamically
+document.getElementById('current-year').textContent = new Date().getFullYear();
